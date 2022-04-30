a73257057871d8eafcd25a446184099300d26020
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-04-30 19:43:56 +0200

    add Layout to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-export default function index() {
-  return (
-    <div>index</div>
-  )
+import Layout from '../components/Layout';
+
+export default function index({ children }) {
+	return <Layout>{children}</Layout>;
 }
