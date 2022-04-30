67819611a29cc56640282e65180c694a7155134a
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-04-30 20:00:45 +0200

    configuration files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,15 @@
 module.exports = {
-  siteMetadata: {
-    title: `adream-site`,
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  plugins: [],
-}
+	siteMetadata: {
+		title: 'aDream site',
+		description: 'recruitment project',
+		copyright: 'This website is copyright 2022 Patrycja Leśniak',
+		design: 'Proudly designed by ADream',
+		siteUrl: `https://adreammain.gatsbyjs.io/`
+	},
+	plugins: [
+		`gatsby-plugin-sass`,
+		`gatsby-plugin-image`,
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`
+	]
+};
