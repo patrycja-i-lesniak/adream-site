9548606a1d078f9ab57208db46a36e003c0715f7
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-05-03 00:20:24 +0200

    change class to className

--- src/components/News.js
+++ src/components/News.js
@@ -5,53 +5,53 @@ import * as styles from './news.module.scss';
 
 export default function News() {
 	return (
-		<div class={styles.container}>
-			<h1 class={styles.h1}>Aktualności</h1>
+		<div className={styles.container}>
+			<h1 className={styles.h1}>Aktualności</h1>
 
-			<div class={styles.wrapper}>
-				<div class={styles.box}>
+			<div className={styles.wrapper}>
+				<div className={styles.box}>
 					<StaticImage
-						class={styles.photo}
+						className={styles.photo}
 						src="../images/News/shutterstock_1433594243.png"
 						alt="photo1"
 					/>
-					<paragraph>01.01.2021</paragraph>
-					<h3 class={styles.h3}>
+					<p className={styles.paragraph}>01.01.2021</p>
+					<h3 className={styles.h3}>
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 					</h3>
-					<paragraph as={Link} class={styles.link} to="/">
+					<p as={Link} className={styles.link} to="/">
 						Więcej
-					</paragraph>
+					</p>
 				</div>
 
-				<div class={styles.box}>
+				<div className={styles.box}>
 					<StaticImage
-						class={styles.photo}
+						className={styles.photo}
 						src="../images/News/shutterstock_1575692173.png"
 						alt="photo1"
 					/>
-					<paragraph>01.01.2021</paragraph>
-					<h3 class={styles.h3}>
+					<p className={styles.paragraph}>01.01.2021</p>
+					<h3 className={styles.h3}>
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 					</h3>
-					<paragraph as={Link} class={styles.link} to="/">
+					<p as={Link} className={styles.link} to="/">
 						Więcej
-					</paragraph>
+					</p>
 				</div>
 
-				<div class={styles.box}>
+				<div className={styles.box}>
 					<StaticImage
-						class={styles.photo}
+						className={styles.photo}
 						src="../images/News/shutterstock_763511722.png"
 						alt="photo1"
 					/>
-					<paragraph>01.01.2021</paragraph>
-					<h3 class={styles.h3}>
+					<p className={styles.paragraph}>01.01.2021</p>
+					<h3 className={styles.h3}>
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 					</h3>
-					<paragraph as={Link} class={styles.link} to="/">
+					<p as={Link} className={styles.link} to="/">
 						Więcej
-					</paragraph>
+					</p>
 				</div>
 			</div>
 		</div>
