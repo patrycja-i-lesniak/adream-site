6408edc4c15fc2d62d135fa17f2b3ea05dd6774a
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-05-02 21:03:49 +0200

    create Layout component and style it

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import '../styles/global.css';
-import * as styles from './layout.module.css';
+import * as styles from './layout.module.scss';
+import '../styles/global.scss';
 
-
-import Header from './Header';
-// import Footer from './Footer';
-
-export default function Layout({ children }) {
+const Layout = ({ children }) => {
 	return (
 		<div className={styles.layout}>
-			<div className={styles.content}>
-				<Header />
-				{children}
-				{/* <Footer /> */}
-			</div>
+			<div>{children}</div>
 		</div>
 	);
-}
+};
+
+export default Layout;
