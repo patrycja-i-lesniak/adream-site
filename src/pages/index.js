3d85ce1e51bd3e2a6fe0c0609b0805c8250dd230
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-05-03 00:21:01 +0200

    add images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { ThemeProvider } from 'react-bootstrap';
 
 import Layout from '../components/Layout';
+import Box from '../components/Box';
+import IconBox from '../components/IconBox';
+import Office from '../components/Office';
+import News from '../components/News';
+import Gallery from '../components/Gallery';
 
 export default function index({ children }) {
+	const breakpoints = [ 'xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs' ];
 	return (
-		<Layout>
-			{children}
-		</Layout>
+		<ThemeProvider breakpoints={breakpoints}>
+			<Layout>
+				<Box />
+				<IconBox />
+				<Office />
+				<News />
+				<Gallery/>
+				{children}
+			</Layout>
+		</ThemeProvider>
 	);
 }
