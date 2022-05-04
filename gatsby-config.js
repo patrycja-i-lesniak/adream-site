534976be20ae5ef1a671c70ad4da90852e978ef9
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-05-04 09:57:39 +0200

    update config files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,10 @@
 module.exports = {
 	siteMetadata: {
-		title: 'aDream site',
-		description: 'recruitment project',
-		copyright: 'This website is copyright 2022 Patrycja Leśniak',
-		design: 'Proudly designed by ADream',
-		siteUrl: `https://adream-site.gatsbyjs.io/`
+		title: `aDream site`,
+		description: `recruitment project`,
+		copyright: `This website is copyright 2022 Patrycja Leśniak`,
+		design: `Proudly designed by ADream`,
+		siteUrl: `https://adreamsite.gtsb.io/`
 	},
 	plugins: [
 		`gatsby-plugin-sass`,
@@ -16,13 +16,6 @@ module.exports = {
 		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
-				name: `projects`,
-				path: `${__dirname}/src/projects/`
-			}
-		},
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
 				name: `images`,
 				path: `${__dirname}/src/images/`
 			}
@@ -30,19 +23,16 @@ module.exports = {
 		{
 			resolve: `gatsby-plugin-google-fonts`,
 			options: {
-				fonts: [
-					`Poppins\:300,400,600,700,900`,
-				],
+				fonts: [ `Poppins\:300,400,600,700,900` ],
 				display: 'swap'
 			}
 		},
 		{
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `data`,
-        path: `${__dirname}/src/data/`,
-        ignore: [`**/\.*`], // ignore files starting with a dot
-      },
-    },
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `src`,
+				path: `${__dirname}/src/`
+			}
+		},
 	]
 };
