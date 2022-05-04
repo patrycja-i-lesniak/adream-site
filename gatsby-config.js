6d002219f095730d27be29e285347021d089d315
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-05-04 06:50:25 +0200

    update config files

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,14 @@ module.exports = {
 				],
 				display: 'swap'
 			}
-		}
+		},
+		{
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`,
+        ignore: [`**/\.*`], // ignore files starting with a dot
+      },
+    },
 	]
 };
