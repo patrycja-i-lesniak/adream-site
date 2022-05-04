1f21ec5d6eb70f3bb3d702a1d931be9451e117ff
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-05-04 16:55:53 +0200

    positioning form elements

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,7 @@ import * as styles from './form.module.scss';
 export default function Form() {
 	return (
 		<div className={styles.container}>
-			<h3 className={styles.h3}>Lorem ipsum Lorem ipsum lorem ipsum</h3>
+			<h3 className={styles.h3}>Lorem ipsum <br/>Lorem ipsum lorem ipsum</h3>
 			<p className={styles.paragraph}>consectetur adipiscing elit. Ut auctor arcu</p>
 			<h4 className={styles.h4}>Zostaw kontakt, zadzwonimy do Ciebie</h4>
 			<form />
