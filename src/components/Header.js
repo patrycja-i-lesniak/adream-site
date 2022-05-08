30bcd36420ac7987b7726a0f9afc81b46b14f6e3
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-05-08 22:00:32 +0200

    clean annecessary header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,20 +7,18 @@ import HeroCarousel from './HeroCarousel';
 
 export default function Header() {
 	return (
-		<div className={styles.wrapper}>
-			<div className={styles.container}>
-				<h1 className={styles.h1}>Firma</h1>
-				<h2 className={styles.h2}>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut auctor
-				</h2>
-				<StaticImage
-					className={styles.progressBar}
-					src="../images/Header/progressBar.png"
-					alt="progress bar"
-				/>
-				<HeroCarousel />
-				<ContactForm />
-			</div>
+		<div className={styles.container}>
+			<h1 className={styles.h1}>Firma</h1>
+			<h2 className={styles.h2}>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut auctor
+			</h2>
+			<StaticImage
+				className={styles.progressBar}
+				src="../images/Header/progressBar.png"
+				alt="progress bar"
+			/>
+			<HeroCarousel />
+			<ContactForm />
 		</div>
 	);
 }
