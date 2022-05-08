a4db38b9e0e119d3f727e3e31f440b2f5f7c39e3
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-05-08 21:47:02 +0200

    format code

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,19 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import * as styles from './layout.module.scss';
 import '../styles/base.css';
 import Header from './Header';
 import Footer from './Footer';
-import Navbar from './Navbar'
- 
+import Navbar from './Navbar';
+
 const Layout = ({ children }) => {
 	return (
-				<div className={styles.layout}>
-					<Navbar/>
-					<Header />
-					<div>{children}</div>
-					<Footer />
-				</div>
+		<div className={styles.layout}>
+			<Navbar />
+			<Header />
+			<div>{children}</div>
+			<Footer />
+		</div>
 	);
 };
 
