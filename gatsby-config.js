6c1262277458dbb7f778098eb967b7c9f167663b
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-05-09 12:39:23 +0200

    update config files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,13 @@
 const path = require(`path`)
 
 module.exports = {
+	pathPrefix: "/patrycja-i-lesniak",
 	siteMetadata: {
 		title: `aDream site`,
 		description: `recruitment project`,
 		copyright: `This website is copyright 2022 Patrycja Leśniak`,
 		design: `Proudly designed by ADream`,
-		siteUrl: `https://adreamsite.gtsb.io/`
+		siteUrl: `https://patrycja-i-lesniak.github.io/adream-site/`
 	},
 	plugins: [
 		`gatsby-plugin-sass`,
