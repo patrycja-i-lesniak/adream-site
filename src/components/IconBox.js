7dbcfe80e9acc0078f7afc7b511799fe13344747
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-05-10 00:40:02 +0200

    rebuild the structure of styles in IconBox component to scss, add import to index.scss file an update index.css file

--- src/components/IconBox.js
+++ src/components/IconBox.js
@@ -1,102 +1,101 @@
 import React from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
 
-import * as styles from './iconBox.module.scss';
 
 export default function IconBox() {
 	return (
-		<div className={styles.container}>
-			<div className={styles.wrapper}>
-				<div className={styles.titleBox}>
-					<h3 className={styles.h3}>Lorem ipsum dolor sit amet, conse</h3>
-					<div className={styles.line} />
+		<div className='iconContainer'>
+			<div className='wrapper'>
+				<div className='titleBox'>
+					<h3 className='h3'>Lorem ipsum dolor sit amet, conse</h3>
+					<div className='line' />
 				</div>
-				<div className={styles.box}>
+				<div className='box'>
 					<div>
 						<StaticImage
-							className={styles.icon}
+							className='icon'
 							src="../static/Icons/Group 603.svg"
 							alt="icon"
 						/>
 					</div>
-					<p className={styles.paragraph}>Lorem ipsum</p>
+					<p className='paragraph'>Lorem ipsum</p>
 				</div>
 
-				<div className={styles.box}>
+				<div className='box'>
 					<div>
 						<StaticImage
-							className={styles.icon}
+							className='icon'
 							src="../static/Icons/Group 602.svg"
 							alt="icon"
 						/>
 					</div>
-					<p className={styles.paragraph}>Lorem ipsum </p>
+					<p className='paragraph'>Lorem ipsum </p>
 				</div>
 
-				<div className={styles.box}>
+				<div className='box'>
 					<div>
 						<StaticImage
-							className={styles.icon}
+							className='icon'
 							src="../static/Icons/Group 601.svg"
 							alt="icon"
 						/>
 					</div>
-					<p className={styles.paragraph}>Lorem ipsum </p>
+					<p className='paragraph'>Lorem ipsum </p>
 				</div>
 
-				<div className={styles.box}>
+				<div className='box'>
 					<div>
 						<StaticImage
-							className={styles.icon}
+							className='icon'
 							src="../static/Icons/Group 604.svg"
 							alt="icon"
 						/>
 					</div>
-					<p className={styles.paragraph}>Lorem ipsum </p>
+					<p className='paragraph'>Lorem ipsum </p>
 				</div>
 
-				<div className={styles.box}>
+				<div className='box'>
 					<div>
 						<StaticImage
-							className={styles.icon}
+							className='icon'
 							src="../static/Icons/Group 598.svg"
 							alt="icon"
 						/>
 					</div>
 
-					<p className={styles.paragraph}>
+					<p className='paragraph'>
 						Lorem ipsum dolor sit amet, conse lorem ipsum lorem
 					</p>
 				</div>
 
-				<div className={styles.box}>
+				<div className='box'>
 					<div>
 						<StaticImage
-							className={styles.icon}
+							className='icon'
 							src="../static/Icons/Group 599.svg"
 							alt="icon"
 						/>
 					</div>
 
-					<p className={styles.paragraph}>
+					<p className='paragraph'>
 						Lorem ipsum dolor sit amet, conse lorem ipsum lorem
 					</p>
 				</div>
 
-				<div className={styles.box}>
+				<div className='box'>
 					<div>
 						<StaticImage
-							className={styles.icon}
+							className='icon'
 							src="../static/Icons/Group 600.svg"
 							alt="icon"
 						/>
 					</div>
 
-					<p className={styles.paragraph}>Lorem ipsum dolor sit amet, conse</p>
+					<p className='paragraph'>Lorem ipsum dolor sit amet, conse</p>
 				</div>
 			</div>
 
-			<button className={styles.button}>
+			<button className='button'>
 				<span>dowiedz się więcej</span>
 				<StaticImage src="../static/Icons/buttonArrow.svg" alt="button arrow" />
 			</button>
