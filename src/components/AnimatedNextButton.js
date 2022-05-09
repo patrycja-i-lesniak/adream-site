f4ba0c3593b076c3afac7bbff8764a57ad839017
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-05-09 21:28:36 +0200

    change scss file name, change import and class name

--- src/components/AnimatedNextButton.js
+++ src/components/AnimatedNextButton.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
 
-import * as styles from './animatedNextButton.module.scss';
+import * as styles from './_animatedNextButton.scss';
 
 export default function AnimatedNextButton() {
 	return (
-		<button className={styles.next}>
+		<button className="next">
 			<StaticImage src="../static/nextSmall.svg" alt="next" />
 		</button>
 	);
