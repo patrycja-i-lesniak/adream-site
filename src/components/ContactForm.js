a5b2b2b847c0b5c945b0cf4348c95736996445dc
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-05-09 22:54:00 +0200

    rebuild the structure of styles in scss, delete unnecessary import

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import * as styles from './contactForm.module.scss';
 
 export default function ContactForm() {
 	return (
-		<div className={`${'container-fluid'} ${styles.container}`}>
-			{/* <div className={styles.container}> */}
+		<div className="contactForm">
+<div className='container-fluid container'>
 			<form action="https://formsubmit.co/patrycja.lesniak@gmail.com" method="POST">
 				<div className="mb-3">
-					<h3 className={styles.h3}>
+					<h3 className='h3'>
 						Lorem ipsum <br />Lorem ipsum lorem ipsum
 					</h3>
-					<p className={styles.paragraph}>consectetur adipiscing elit. Ut auctor arcu</p>
-					<h4 className={styles.h4}>Zostaw kontakt, zadzwonimy do Ciebie</h4>
+					<p className='paragraph'>consectetur adipiscing elit. Ut auctor arcu</p>
+					<h4 className='h4'>Zostaw kontakt, zadzwonimy do Ciebie</h4>
 
 					<input
-						className={`${'form-control'} ${styles.input}`}
+						className='form-control input'
 						type="text"
 						name="name"
 						required
@@ -23,7 +22,7 @@ export default function ContactForm() {
 					/>
 
 					<input
-						className={`${'form-control'} ${styles.input}`}
+						className='form-control input'
 						type="text"
 						name="phone"
 						required
@@ -35,39 +34,41 @@ export default function ContactForm() {
 						type="email"
 						name="email"
 						required
-						className={`${'form-control'} ${styles.input}`}
+						className='form-control input'
 						placeholder="Email"
 						aria-describedby="email input"
 					/>
 
 					<input
-						className={`${'form-control'} ${styles.input}`}
+						className='form-control input'
 						rows="1"
 						name="textarea"
 						required
 						placeholder="Lorem ipsum lorem ipsum"
 						aria-describedby="text input"
 					/>
-					<div className={styles.checkboxWrapper}>
+					<div className='checkboxWrapper'>
 						<input
 							type="checkbox"
 							name="checkbox"
 							required
-							className={`${'form-check-input'} ${styles.checkbox}`}
+							className='form-check-input checkbox'
 							id="check"
 						/>
-						<div className={`${'form-text'} ${styles.text}`}>
+						<div className='form-text  text'>
 							Wyrażam dobrowolną zgodę na przetwarzanie moich danych osobowych
 							więcej...
 						</div>
 					</div>
 
-					<button type="submit" className={styles.button}>
+					<button type="submit" className='button'>
 						<span>wyślij</span>
 					</button>
 				</div>
 			</form>
-			{/* </div> */}
+			
 		</div>
+		</div>
+		
 	);
 }
