395ec3387058420f551d76e38d3add0e672902dd
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-05-10 00:42:35 +0200

    rebuild the structure of styles in Layout component to scss, add import to index.scss file an update index.css file

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
-import * as styles from './layout.module.scss';
 import Header from './Header';
 import Footer from './Footer';
 import Navbar from './Navbar';
 
 const Layout = ({ children }) => {
 	return (
-		<div className={styles.layout}>
+		<div className='layout'>
 			<Navbar />
 			<Header />
 			<div>{children}</div>
