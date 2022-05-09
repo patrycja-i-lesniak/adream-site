bae645009c31da9000d03c26ed25e50918719806
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-05-09 23:08:24 +0200

    rebuild the structure of styles in footer component to scss, add import to index.scss file an update index.css file

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,58 +2,59 @@ import React from 'react';
 import { Link } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
 
-import * as styles from './footer.module.scss';
-
 export default function Footer() {
 	return (
-		<div className={styles.container}>
-		<div className={styles.graphicContainer}><StaticImage
-				className={styles.graphic}
-				src="../static/Footer/Group 586@2x.png"
-				alt="graphic"
-			/>
-		</div>
-			
+		<div className="footer">
+			<div className="container">
+				<div className="graphicContainer">
+					<StaticImage
+						className="graphic"
+						src="../static/Footer/Group 586@2x.png"
+						alt="graphic"
+					/>
+				</div>
+
 				<div>
-					<p className={styles.logo}>logo</p>
-					<p className={styles.paragraph}>
+					<p className="logo">logo</p>
+					<p className="paragraph">
 						ul. Towarowa 5/6<br />31-000 Kraków
 					</p>
-					<p className={styles.paragraphBold}>
+					<p className="paragraph bold">
 						+48 999 999 999<br />email@email.com
 					</p>
 				</div>
-				<p className={styles.policy}>Polityka prywatności</p>
-			<div className={styles.footerNavigation}>
-				<div className={styles.nav}>
-					<Link className={styles.link} to="/">
-						Strona główna
-					</Link>
-					<Link className={styles.link} to="/">
-						Poznaj przestrzeń
-					</Link>
-					<Link className={styles.link} to="/offer">
-						Oferta
-					</Link>
+				<p className="policy">Polityka prywatności</p>
+				<div className="footerNavigation">
+					<div className="nav">
+						<Link className="link" to="/">
+							Strona główna
+						</Link>
+						<Link className="link" to="/">
+							Poznaj przestrzeń
+						</Link>
+						<Link className="link" to="/offer">
+							Oferta
+						</Link>
+					</div>
+					<div className="nav">
+						<Link className="link" to="/location">
+							Lokalizacja
+						</Link>
+						<Link className="link" to="/office">
+							Własne biuro
+						</Link>
+						<Link className="link" to="/contact">
+							Kontakt
+						</Link>
+					</div>
 				</div>
-				<div className={styles.nav}>
-					<Link className={styles.link} to="/location">
-						Lokalizacja
-					</Link>
-					<Link className={styles.link} to="/office">
-						Własne biuro
-					</Link>
-					<Link className={styles.link} to="/contact">
-						Kontakt
-					</Link>
-				</div>
-			</div>
-				<p className={styles.info}>Proudly designed by</p>
+				<p className="info">Proudly designed by</p>
 				<StaticImage
-					className={styles.aDreamLogo}
+					className="aDreamLogo"
 					src="../static/Footer/logo_adream@2x.png"
 					alt="logo"
 				/>
+			</div>
 		</div>
 	);
 }
