fc3104fd0828490743adbd7ed25740b10b03aac9
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-05-10 01:26:59 +0200

    rebuild the structure of styles in Navbar component to scss, add import to index.scss file an update index.css file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,49 +3,45 @@ import { Link } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
 import { CgMenu } from 'react-icons/cg';
 
-import * as styles from './navbar.module.scss';
-
 export default function Navbar() {
 	return (
-	
-			<div className={styles.wrapper}>
-				<div>
-					<Link className={styles.logo} to="/">
-						Logo
-					</Link>
-				</div>
-				<div className={styles.links}>
-					<Link className={styles.link} to="/">
-						Poznaj przestrzeń
-					</Link>
-					<Link className={styles.link} to="/offer">
-						Oferta
-					</Link>
-					<Link className={styles.link} to="/location">
-						Lokalizacja
-					</Link>
-
-					<Link className={styles.link} to="/office">
-						Własne biuro
-					</Link>
-					<Link className={styles.link} to="/contact">
-						Kontakt
-					</Link>
-				</div>
-				<div className={styles.socials}>
-					<Link className={styles.link} to="/">
-						<StaticImage src="../static/facebook.png" alt="facebook" />
-					</Link>
-					<Link className={styles.link} to="/">
-						<StaticImage src="../static/instagram.png" alt="instagram" />
-					</Link>
-				</div>
-				<div>
-					<Link className={styles.hamburger} to="/">
-						<CgMenu />
-					</Link>
-				</div>
+		<div className="navbarWrapper">
+			<div className='logoWrapper'>
+				<Link className="logo" to="/">
+					Logo
+				</Link>
 			</div>
+			<div className="links">
+				<Link className="link" to="/">
+					Poznaj przestrzeń
+				</Link>
+				<Link className="link" to="/offer">
+					Oferta
+				</Link>
+				<Link className="link" to="/location">
+					Lokalizacja
+				</Link>
 
+				<Link className="link" to="/office">
+					Własne biuro
+				</Link>
+				<Link className="link" to="/contact">
+					Kontakt
+				</Link>
+			</div>
+			<div className="socials">
+				<Link className="link" to="/">
+					<StaticImage src="../static/facebook.png" alt="facebook" />
+				</Link>
+				<Link className="link" to="/">
+					<StaticImage src="../static/instagram.png" alt="instagram" />
+				</Link>
+			</div>
+			<div>
+				<Link className="hamburger" to="/">
+					<CgMenu />
+				</Link>
+			</div>
+		</div>
 	);
 }
