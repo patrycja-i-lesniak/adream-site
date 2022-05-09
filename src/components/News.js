998b9553f00e3890424cafbcface8c95245cee00
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-05-10 01:32:01 +0200

    rebuild the structure of styles in News component to scss, add import to index.scss file an update index.css file

--- src/components/News.js
+++ src/components/News.js
@@ -1,55 +1,54 @@
 import React from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
 import { Link } from 'gatsby';
-import * as styles from './news.module.scss';
 
 export default function News() {
 	return (
-		<div className={styles.container}>
-			<h1 className={styles.h1}>Aktualności</h1>
+		<div className='newsContainer'>
+			<h1 className='h1'>Aktualności</h1>
 
-			<div className={styles.wrapper}>
-				<div className={styles.box}>
+			<div className='wrapper'>
+				<div className='box'>
 					<StaticImage
-						className={styles.photo}
+						className='photo'
 						src="../images/News/shutterstock_1433594243.png"
 						alt="photo1"
 					/>
-					<p className={styles.paragraph}> 01.01.2021</p>
-					<h3 className={styles.h3}>
+					<p className='paragraph'> 01.01.2021</p>
+					<h3 className='h3'>
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 					</h3>
-					<p as={Link} className={styles.link} to="/">
+					<p as={Link} className='link' to="/">
 						Więcej
 					</p>
 				</div>
 
-				<div className={styles.box}>
+				<div className='box'>
 					<StaticImage
-						className={styles.photo}
+						className='photo'
 						src="../images/News/shutterstock_1575692173.png"
 						alt="photo1"
 					/>
-					<p className={styles.paragraph}>01.01.2021</p>
-					<h3 className={styles.h3}>
+					<p className='paragraph'>01.01.2021</p>
+					<h3 className='h3'>
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 					</h3>
-					<p as={Link} className={styles.link} to="/">
+					<p as={Link} className='link' to="/">
 						Więcej
 					</p>
 				</div>
 
-				<div className={styles.box}>
+				<div className='box'>
 					<StaticImage
-						className={styles.photo}
+						className='photo'
 						src="../images/News/shutterstock_763511722.png"
 						alt="photo1"
 					/>
-					<p className={styles.paragraph}>01.01.2021</p>
-					<h3 className={styles.h3}>
+					<p className='paragraph'>01.01.2021</p>
+					<h3 className='h3'>
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 					</h3>
-					<p as={Link} className={styles.link} to="/">
+					<p as={Link} className='link' to="/">
 						Więcej
 					</p>
 				</div>
