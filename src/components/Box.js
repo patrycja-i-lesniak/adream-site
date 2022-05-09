0f011e5aa9df4830fabc6037109863d0af3c7f01
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-05-09 22:38:59 +0200

    rebuild the structure of styles to scss

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
-import '../styles/_box.scss';
+
 import Img from 'gatsby-image';
 import AnimatedNextButton from './AnimatedNextButton';
 
@@ -42,97 +42,99 @@ export default function Box() {
 		}
 	`);
 	return (
-		<div className="container">
-			<div className="box">
-				<div className="imgHoverZoom">
-					<Img
-						className="image"
-						fluid={data.box1.childImageSharp.fluid}
-						alt="This is the first Box"
-					/>
-				</div>
+		<div className="box">
+			<div className="container">
+				<div className="smallBox">
+					<div className="imgHoverZoom">
+						<Img
+							className="image"
+							fluid={data.box1.childImageSharp.fluid}
+							alt="This is the first Box"
+						/>
+					</div>
 
-				<div className="content">
-					<div className="yellowBox" />
+					<div className="content">
+						<div className="yellowBox" />
 
-					<div className="info">
-						<h3 className="h3">Biuro</h3>
-						<div className="infoBox">
-							<div className="line" />
-							<p className="paragraph">od 1000 zł/msc</p>
+						<div className="info">
+							<h3 className="h3">Biuro</h3>
+							<div className="infoBox">
+								<div className="line" />
+								<p className="paragraph">od 1000 zł/msc</p>
+							</div>
 						</div>
 					</div>
-				</div>
-				<AnimatedNextButton />
-			</div>
-
-			<div className="box">
-				<div className="imgHoverZoom">
-					<Img
-						className="image"
-						fluid={data.box2.childImageSharp.fluid}
-						alt="This is the second Box"
-					/>
+					<AnimatedNextButton />
 				</div>
 
-				<div className="content">
-					<div className="yellowBox" />
-					<div className="info">
-						<h3 className="h3">
-							Lorem <br />ipsum
-						</h3>
-						<div className="infoBox">
-							<div className="line" />
-							<p className="paragraph">od 500 zł/msc</p>
+				<div className="smallBox">
+					<div className="imgHoverZoom">
+						<Img
+							className="image"
+							fluid={data.box2.childImageSharp.fluid}
+							alt="This is the second Box"
+						/>
+					</div>
+
+					<div className="content">
+						<div className="yellowBox" />
+						<div className="info">
+							<h3 className="h3">
+								Lorem <br />ipsum
+							</h3>
+							<div className="infoBox">
+								<div className="line" />
+								<p className="paragraph">od 500 zł/msc</p>
+							</div>
 						</div>
 					</div>
-				</div>
-				<AnimatedNextButton />
-			</div>
-
-			<div className="box">
-				<div className="imgHoverZoom">
-					<Img
-						className="image"
-						fluid={data.box3.childImageSharp.fluid}
-						alt="This is the third Box"
-					/>
+					<AnimatedNextButton />
 				</div>
 
-				<div className="content bottomBox">
-					<div className="yellowBox" />
-					<div className="info">
-						<h3 className="h3">
-							Biuro <br />lorem
-						</h3>
-						<div className="infoBox">
-							<div className="line" />
-							<p className="paragraph">od 1000zł/h</p>
+				<div className="smallBox">
+					<div className="imgHoverZoom">
+						<Img
+							className="image"
+							fluid={data.box3.childImageSharp.fluid}
+							alt="This is the third Box"
+						/>
+					</div>
+
+					<div className="content bottomBox">
+						<div className="yellowBox" />
+						<div className="info">
+							<h3 className="h3">
+								Biuro <br />lorem
+							</h3>
+							<div className="infoBox">
+								<div className="line" />
+								<p className="paragraph">od 1000zł/h</p>
+							</div>
 						</div>
 					</div>
+					<AnimatedNextButton />
 				</div>
-				<AnimatedNextButton />
-			</div>
 
-			<div className="box">
-				<div className="imgHoverZoom">
-					<Img
-						className="image"
-						fluid={data.box4.childImageSharp.fluid}
-						alt="This is the fourth Box"
-					/>
-				</div>
-				<div className="content bottomBox">
-					<div className="yellowBox" />
-					<div className="info">
-						<h3 className="h3">Biuro lorem ipsum Lorem</h3>
-						<div className="infoBox">
-							<div className="line" />
-							<p className="paragraph">od 100 zł/h</p>
+				<div className="smallBox">
+					<div className="imgHoverZoom">
+						<Img
+							className="image"
+							fluid={data.box4.childImageSharp.fluid}
+							alt="This is the fourth Box"
+						/>
+					</div>
+					<div className="content bottomBox">
+						<div className="yellowBox" />
+						<div className="info">
+							<h3 className="h3">Biuro lorem ipsum Lorem</h3>
+							<div className="infoBox">
+								<div className="line" />
+								<p className="paragraph">od 100 zł/h</p>
+							</div>
 						</div>
 					</div>
+					<AnimatedNextButton />
 				</div>
-				<AnimatedNextButton />
 			</div>
 		</div>
 	);
