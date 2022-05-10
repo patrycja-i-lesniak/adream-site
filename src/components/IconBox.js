37c1f368eca72d1e8cebd5fefb0d704094df0e39
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-05-10 10:42:28 +0200

    change class names to specific for an IconBox component

--- src/components/IconBox.js
+++ src/components/IconBox.js
@@ -4,98 +4,98 @@ import { StaticImage } from 'gatsby-plugin-image';
 
 export default function IconBox() {
 	return (
-		<div className='iconContainer'>
-			<div className='wrapper'>
-				<div className='titleBox'>
-					<h3 className='h3'>Lorem ipsum dolor sit amet, conse</h3>
-					<div className='line' />
+		<div className='icon-container'>
+			<div className='icon-wrapper'>
+				<div className='icon-title'>
+					<h3 className='icon-h3'>Lorem ipsum dolor sit amet, conse</h3>
+					<div className='icon-line' />
 				</div>
-				<div className='box'>
+				<div className='icon-box'>
 					<div>
 						<StaticImage
-							className='icon'
+							className='icon-image'
 							src="../static/Icons/Group 603.svg"
 							alt="icon"
 						/>
 					</div>
-					<p className='paragraph'>Lorem ipsum</p>
+					<p className='icon-paragraph'>Lorem ipsum</p>
 				</div>
 
-				<div className='box'>
+				<div className='icon-box'>
 					<div>
 						<StaticImage
-							className='icon'
+							className='icon-image'
 							src="../static/Icons/Group 602.svg"
 							alt="icon"
 						/>
 					</div>
-					<p className='paragraph'>Lorem ipsum </p>
+					<p className='icon-paragraph'>Lorem ipsum </p>
 				</div>
 
-				<div className='box'>
+				<div className='icon-box'>
 					<div>
 						<StaticImage
-							className='icon'
+							className='icon-image'
 							src="../static/Icons/Group 601.svg"
 							alt="icon"
 						/>
 					</div>
-					<p className='paragraph'>Lorem ipsum </p>
+					<p className='icon-paragraph'>Lorem ipsum </p>
 				</div>
 
-				<div className='box'>
+				<div className='icon-box'>
 					<div>
 						<StaticImage
-							className='icon'
+							className='icon-image'
 							src="../static/Icons/Group 604.svg"
 							alt="icon"
 						/>
 					</div>
-					<p className='paragraph'>Lorem ipsum </p>
+					<p className='icon-paragraph'>Lorem ipsum </p>
 				</div>
 
-				<div className='box'>
+				<div className='icon-box'>
 					<div>
 						<StaticImage
-							className='icon'
+							className='icon-image'
 							src="../static/Icons/Group 598.svg"
 							alt="icon"
 						/>
 					</div>
 
-					<p className='paragraph'>
+					<p className='icon-paragraph'>
 						Lorem ipsum dolor sit amet, conse lorem ipsum lorem
 					</p>
 				</div>
 
-				<div className='box'>
+				<div className='icon-box'>
 					<div>
 						<StaticImage
-							className='icon'
+							className='icon-image'
 							src="../static/Icons/Group 599.svg"
 							alt="icon"
 						/>
 					</div>
 
-					<p className='paragraph'>
+					<p className='icon-paragraph'>
 						Lorem ipsum dolor sit amet, conse lorem ipsum lorem
 					</p>
 				</div>
 
-				<div className='box'>
+				<div className='icon-box'>
 					<div>
 						<StaticImage
-							className='icon'
+							className='icon-image'
 							src="../static/Icons/Group 600.svg"
 							alt="icon"
 						/>
 					</div>
 
-					<p className='paragraph'>Lorem ipsum dolor sit amet, conse</p>
+					<p className='icon-paragraph'>Lorem ipsum dolor sit amet, conse</p>
 				</div>
 			</div>
 
-			<button className='button'>
+			<button className='icon-button'>
 				<span>dowiedz się więcej</span>
 				<StaticImage src="../static/Icons/buttonArrow.svg" alt="button arrow" />
 			</button>
