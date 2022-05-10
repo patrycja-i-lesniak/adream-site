889d63a5de3db04d5031012a49d4aa95f8328a82
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-05-10 10:51:38 +0200

    change class names to specific for a News component

--- src/components/News.js
+++ src/components/News.js
@@ -4,51 +4,51 @@ import { Link } from 'gatsby';
 
 export default function News() {
 	return (
-		<div className='newsContainer'>
-			<h1 className='title'>Aktualności</h1>
+		<div className='news-container'>
+			<h1 className='news-h1'>Aktualności</h1>
 
-			<div className='newsWrapper'>
-				<div className='box'>
+			<div className='news-wrapper'>
+				<div className='news-box'>
 					<StaticImage
-						className='photo'
+						className='news-photo'
 						src="../images/News/shutterstock_1433594243.png"
 						alt="photo1"
 					/>
-					<p className='paragraph'> 01.01.2021</p>
-					<h3 className='h3'>
+					<p className='news-paragraph'> 01.01.2021</p>
+					<h3 className='news-h3'>
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 					</h3>
-					<p as={Link} className='link' to="/">
+					<p as={Link} className='news-link' to="/">
 						Więcej
 					</p>
 				</div>
 
-				<div className='box'>
+				<div className='news-box'>
 					<StaticImage
-						className='photo'
+						className='news-photo'
 						src="../images/News/shutterstock_1575692173.png"
 						alt="photo1"
 					/>
-					<p className='paragraph'>01.01.2021</p>
-					<h3 className='h3'>
+					<p className='news-paragraph'>01.01.2021</p>
+					<h3 className='news-h3'>
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 					</h3>
-					<p as={Link} className='link' to="/">
+					<p as={Link} className='news-link' to="/">
 						Więcej
 					</p>
 				</div>
 
-				<div className='box'>
+				<div className='news-box'>
 					<StaticImage
-						className='photo'
+						className='news-photo'
 						src="../images/News/shutterstock_763511722.png"
 						alt="photo1"
 					/>
-					<p className='paragraph'>01.01.2021</p>
-					<h3 className='h3'>
+					<p className='news-paragraph'>01.01.2021</p>
+					<h3 className='news-h3'>
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 					</h3>
-					<p as={Link} className='link' to="/">
+					<p as={Link} className='news-link' to="/">
 						Więcej
 					</p>
 				</div>
