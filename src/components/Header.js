2d1af2b31f10596ea16e5723f28a2c89a8610788
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-05-10 10:31:39 +0200

    rename the classes as specific to the Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,14 @@ import HeroCarousel from './HeroCarousel';
 
 export default function Header() {
 	return (
-		<div className='header'>
-			<div className='wrapper'>
-				<h1 className='headerTitle'>Firma</h1>
-				<h2 className='h2'>
+		<div className="header">
+			<div className="header-wrapper">
+				<h1 className="header-h1">Firma</h1>
+				<h2 className="header-h2">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut auctor
 				</h2>
 				<StaticImage
-					className='progressBar'
+					className="header-progress-bar"
 					src="../images/Header/progressBar.png"
 					alt="progress bar"
 				/>
