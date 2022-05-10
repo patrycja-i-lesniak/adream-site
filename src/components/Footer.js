a924728c71dabef65cd83d965e877708be2e541a
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-05-10 10:08:28 +0200

    assign classes with new names specific to the Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,52 +5,52 @@ import { StaticImage } from 'gatsby-plugin-image';
 export default function Footer() {
 	return (
 		<div className="footer">
-			<div className="container">
-				<div className="graphicContainer">
+			<div className="footer-container">
+				<div className="footer-graphic">
 					<StaticImage
-						className="graphic"
+						className="footer-image"
 						src="../static/Footer/Group 586@2x.png"
 						alt="graphic"
 					/>
 				</div>
 
 				<div>
-					<p className="logo">logo</p>
-					<p className="paragraph">
+					<p className="footer-logo">logo</p>
+					<p className="footer-paragraph">
 						ul. Towarowa 5/6<br />31-000 Kraków
 					</p>
-					<p className="paragraph bold">
+					<p className="footer-paragraph footer-bold">
 						+48 999 999 999<br />email@email.com
 					</p>
 				</div>
-				<p className="policy">Polityka prywatności</p>
-				<div className="footerNavigation">
-					<div className="nav">
-						<Link className="link" to="/">
+				<p className="footer-policy">Polityka prywatności</p>
+				<div className="footer-navigation">
+					<div className="footer-link-box">
+						<Link className="footer-link" to="/">
 							Strona główna
 						</Link>
-						<Link className="link" to="/">
+						<Link className="footer-link" to="/">
 							Poznaj przestrzeń
 						</Link>
-						<Link className="link" to="/offer">
+						<Link className="footer-link" to="/offer">
 							Oferta
 						</Link>
 					</div>
-					<div className="nav">
-						<Link className="link" to="/location">
+					<div className="footer-link-box">
+						<Link className="footer-link" to="/location">
 							Lokalizacja
 						</Link>
-						<Link className="link" to="/office">
+						<Link className="footer-link" to="/office">
 							Własne biuro
 						</Link>
-						<Link className="link" to="/contact">
+						<Link className="footer-link" to="/contact">
 							Kontakt
 						</Link>
 					</div>
 				</div>
-				<p className="info">Proudly designed by</p>
+				<p className="footer-info">Proudly designed by</p>
 				<StaticImage
-					className="aDreamLogo"
+					className="footer-design"
 					src="../static/Footer/logo_adream@2x.png"
 					alt="logo"
 				/>
