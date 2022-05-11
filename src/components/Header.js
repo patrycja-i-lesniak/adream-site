846309335d45ffc6be9e0a64cd71672708d0117c
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-05-11 22:10:09 +0200

    correct text orientation in header for breakpoints

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,17 +8,18 @@ export default function Header() {
 	return (
 		<div className="header">
 			<div className="header-wrapper">
-				<h1 className="header-h1">Firma</h1>
-				<h2 className="header-h2">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut auctor
-				</h2>
 				<StaticImage
 					className="header-progress-bar"
 					src="../images/Header/progressBar.png"
 					alt="progress bar"
 				/>
 				<Carousel />
+				<h1 className="header-h1">Firma</h1>
+				<h2 className="header-h2">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut auctor
+				</h2>
 				<Form />
+				
 			</div>
 		</div>
 	);
