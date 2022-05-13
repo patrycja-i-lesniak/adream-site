3cab3ccfe45c783825d90998a1fb99e9d36badee
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-05-13 19:38:31 +0200

    rebuild the footer and adapt it to mobile views

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,47 +13,51 @@ export default function Footer() {
 						alt="graphic"
 					/>
 				</div>
+				<div className="footer-text">
+					<div className="footer-text-right">
+						<h1 className="footer-logo">logo</h1>
+						<p className="footer-paragraph">
+							ul. Towarowa 5/6<br />31-000 Kraków
+						</p>
+						<p className="footer-paragraph bold">
+							+48 999 999 999<br />email@email.com
+						</p>
+						<p className="footer-policy">Polityka prywatności</p>
+					</div>
 
-				<div>
-					<p className="footer-logo">logo</p>
-					<p className="footer-paragraph">
-						ul. Towarowa 5/6<br />31-000 Kraków
-					</p>
-					<p className="footer-paragraph footer-bold">
-						+48 999 999 999<br />email@email.com
-					</p>
-				</div>
-				<p className="footer-policy">Polityka prywatności</p>
-				<div className="footer-navigation">
-					<div className="footer-link-box">
-						<Link className="footer-link" to="/">
-							Strona główna
-						</Link>
-						<Link className="footer-link" to="/">
-							Poznaj przestrzeń
-						</Link>
-						<Link className="footer-link" to="/offer">
-							Oferta
-						</Link>
+					<div className="footer-navigation">
+						<div className="footer-link-box">
+							<Link className="footer-link" to="/">
+								Strona główna
+							</Link>
+							<Link className="footer-link" to="/">
+								Poznaj przestrzeń
+							</Link>
+							<Link className="footer-link" to="/offer">
+								Oferta
+							</Link>
+						</div>
+						<div className="footer-link-box">
+							<Link className="footer-link" to="/location">
+								Lokalizacja
+							</Link>
+							<Link className="footer-link" to="/office">
+								Własne biuro
+							</Link>
+							<Link className="footer-link" to="/contact">
+								Kontakt
+							</Link>
+						</div>
+						<div className="footer-design">
+							<p className="footer-design-text">Proudly designed by</p>
+							<StaticImage
+								className="footer-design-image"
+								src="../static/Footer/logo_adream@2x.png"
+								alt="logo"
+							/>
+						</div>
 					</div>
-					<div className="footer-link-box">
-						<Link className="footer-link" to="/location">
-							Lokalizacja
-						</Link>
-						<Link className="footer-link" to="/office">
-							Własne biuro
-						</Link>
-						<Link className="footer-link" to="/contact">
-							Kontakt
-						</Link>
-					</div>
 				</div>
-				<p className="footer-info">Proudly designed by</p>
-				<StaticImage
-					className="footer-design"
-					src="../static/Footer/logo_adream@2x.png"
-					alt="logo"
-				/>
 			</div>
 		</div>
 	);
