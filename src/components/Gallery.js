04bf46ae3b4a00dd2a31bb567595444f5a799f5d
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-05-16 16:50:31 +0200

    rebuild gallery, add graphql

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
-import { graphql, useStaticQuery, Link } from 'gatsby';
+import { graphql, useStaticQuery } from 'gatsby';
 import SimpleReactLightbox, { SRLWrapper } from 'simple-react-lightbox';
 import { FiArrowRight, FiArrowLeft } from 'react-icons/fi';
 
@@ -12,95 +12,67 @@ export default function Gallery() {
 	const slide = (shift) => {
 		scrl.current.scrollLeft += shift;
 		setscrollX(scrollX + shift);
-
-		if (
-			Math.floor(scrl.current.scrollWidth - scrl.current.scrollLeft) <=
-			scrl.current.offsetWidth
-		) {
-			setscrolEnd(true);
-		} else {
-			setscrolEnd(false);
-		}
+		Math.floor(scrl.current.scrollWidth - scrl.current.scrollLeft) <= scrl.current.offsetWidth
+			? setscrolEnd(true)
+			: setscrolEnd(false);
 	};
 
 	const scrollCheck = () => {
 		setscrollX(scrl.current.scrollLeft);
-		if (
-			Math.floor(scrl.current.scrollWidth - scrl.current.scrollLeft) <=
-			scrl.current.offsetWidth
-		) {
-			setscrolEnd(true);
-		} else {
-			setscrolEnd(false);
-		}
+		Math.floor(scrl.current.scrollWidth - scrl.current.scrollLeft) <= scrl.current.offsetWidth
+			? setscrolEnd(true)
+			: setscrolEnd(false);
 	};
 
-	// const data = useStaticQuery(graphql`
-	// 	query {
-	// 		gallery: allFile(
-	// 			filter: { relativeDirectory: { eq: "Gallery" } }
-	// 			sort: { fields: base, order: ASC }
-	// 		) {
-	// 			edges {
-	// 				node {
-	// 					id
-	// 					base
-	// 					publicURL
-	// 					childImageSharp {
-	// 						gatsbyImageData(
-	// 							transformOptions: { fit: COVER }
-	// 							placeholder: BLURRED
-	// 							webpOptions: { quality: 50 }
-	// 						)
-	// 					}
-	// 				}
-	// 			}
-	// 		}
-	// 		featured: allFile(
-	// 			filter: { relativeDirectory: { eq: "Gallery/featured" } }
-	// 			sort: { fields: base, order: ASC }
-	// 		) {
-	// 			edges {
-	// 				node {
-	// 					id
-	// 					base
-	// 					publicURL
-	// 					childImageSharp {
-	// 						gatsbyImageData(
-	// 							transformOptions: { fit: COVER }
-	// 							placeholder: BLURRED
-	// 							webpOptions: { quality: 50 }
-	// 						)
-	// 					}
-	// 				}
-	// 			}
-	// 		}
-	// 	}
-	// `);
-
-	const options = {
-		overlayColor: 'rgb(25, 136, 124)',
-		captionStyle: {
-			captionColor: '#FFFFFF',
-			captionFontFamily: 'Poppins, sans-serif',
-			captionFontSize: '40px',
-			captionFontStyle: 'capitalize'
-		},
-		autoplaySpeed: 2500,
-		transitionSpeed: 900,
-		buttonsStyle: {
-			buttonsBackgroundColor: '#FFFFFF',
-			buttonsIconColor: '#000000'
+	const {thumbs, featured} = useStaticQuery(graphql`
+		query {
+			thumbs: allFile(
+				filter: { relativeDirectory: { eq: "Gallery/thumbs" } }
+				sort: { fields: base, order: ASC }
+			) {
+				edges {
+					node {
+						id
+						base
+						publicURL
+						childImageSharp {
+							gatsbyImageData(
+								transformOptions: { fit: COVER }
+								placeholder: BLURRED
+								webpOptions: { quality: 50 }
+							)
+						}
+					}
+				}
+			}
+			featured: allFile(
+				filter: { relativeDirectory: { eq: "Gallery/featured" } }
+				sort: { fields: base, order: ASC }
+			) {
+				edges {
+					node {
+						id
+						base
+						publicURL
+						childImageSharp {
+							gatsbyImageData(
+								transformOptions: { fit: COVER }
+								placeholder: BLURRED
+								webpOptions: { quality: 25 }
+							)
+						}
+					}
+				}
+			}
 		}
-	};
+	`);
 
 	return (
 		<div className="gallery">
 			<SimpleReactLightbox>
 				<h1 className="gallery-h1">Galeria</h1>
 				<div className="gallery-line" />
-
-				<SRLWrapper options={options}>
+				<SRLWrapper>
 					<div
 						id="container"
 						className="scrolling-wrapper"
@@ -108,75 +80,86 @@ export default function Gallery() {
 						onScroll={scrollCheck}
 					>
 						<div className="card gallery-zoom">
-							<Link to="../images/Gallery/featured/image1.webp" data-attribute="SRL">
+							<a
+								href={featured.edges[0].node.publicURL}
+								data-attribute="SRL"
+							>
 								<div className="gallery-zoom">
 									<StaticImage
+										srl_gallery_image="true"
 										className="gallery-image"
 										alt="This is the 1 photo"
-										src="../images/Gallery/thumbs/thumb1.webp"
+										src='../images/Gallery/thumbs/thumb1.webp'
 										style={({ height: '888px' }, { width: '625px' })}
 									/>
 								</div>
-							</Link>
+							</a>
 						</div>
 						<div className="card gallery-zoom">
-							<Link to="../images/Gallery/featured/image2.webp" data-attribute="SRL">
+							<a
+								href={featured.edges[1].node.publicURL}
+								data-attribute="SRL"
+							>
 								<div className="gallery-zoom">
 									<StaticImage
+										srl_gallery_image="true"
 										className="gallery-image"
 										alt="This is the 2 photo"
 										src="../images/Gallery/thumbs/thumb2.webp"
 									/>
 								</div>
-							</Link>
+							</a>
 						</div>
 						<div className="card gallery-zoom">
-							<Link to="../images/Gallery/featured/image3.webp" data-attribute="SRL">
+							<a href={featured.edges[2].node.publicURL} data-attribute="SRL">
 								<div className="gallery-zoom">
 									<StaticImage
+										srl_gallery_image="true"
 										className="gallery-image"
 										alt="This is the 3 photo"
 										src="../images/Gallery/thumbs/thumb3.webp"
 									/>
 								</div>
-							</Link>
+							</a>
 						</div>
 						<div className="card gallery-zoom double">
-							{' '}
 							<div className="white-box" />
-							<Link to="../images/Gallery/featured/image5.webp" data-attribute="SRL">
+							<a href={featured.edges[4].node.publicURL} data-attribute="SRL">
 								<div className="gallery-zoom">
 									<StaticImage
+										srl_gallery_image="true"
 										className="gallery-image"
 										alt="This is the 5 photo"
 										src="../images/Gallery/thumbs/thumb5.webp"
 										style={{ height: '290px' }}
 									/>
 								</div>
-							</Link>
+							</a>
 							<div className="white-box" style={{ width: '600px' }} />
 						</div>
 						<div className="card gallery-zoom double">
-							<Link to="../images/Gallery/featured/image4.webp" data-attribute="SRL">
+							<a href={featured.edges[3].node.publicURL} data-attribute="SRL">
 								<div className="gallery-zoom">
 									<StaticImage
+										srl_gallery_image="true"
 										className="gallery-image"
 										alt="This is the 4 photo"
 										src="../images/Gallery/thumbs/thumb4.webp"
 										style={{ height: '577px' }}
 									/>
 								</div>
-							</Link>
-							<Link to="../images/Gallery/featured/image6.webp" data-attribute="SRL">
+							</a>
+							<a href={featured.edges[5].node.publicURL} data-attribute="SRL">
 								<div className="gallery-zoom">
 									<StaticImage
+										srl_gallery_image="true"
 										className="gallery-image"
 										alt="This is the 6 photo"
 										src="../images/Gallery/thumbs/thumb6.webp"
 										style={({ width: '1227' }, { height: '577px' })}
 									/>
 								</div>
-							</Link>
+							</a>
 						</div>
 					</div>
 				</SRLWrapper>
