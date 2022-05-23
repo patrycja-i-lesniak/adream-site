a5b1324924f33fbfb9bf4b89e2437517853f80d5
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-05-23 18:11:26 +0200

    add to Carousel current and total slides add a function that controls the progress bar operation after clicking the button and a function that gets the index of the current slide

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,22 +1,70 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
 import { StaticImage } from 'gatsby-plugin-image';
 import { FiArrowRight, FiArrowLeft } from 'react-icons/fi';
 
 export default function Carousel() {
+	const totalSlides = 3;
+	const interval = '10000';
+	const [ isActive, setActive ] = useState('false');
+	const [ isPlaying, setPlaying ] = useState('true');
+	const [ currentSlide, setCurrentSlide ] = useState(1);
+
+	const handleToggle = () => {
+		setActive(!isActive);
+		setCurrentSlide();
+	};
+
+	useEffect(
+		() => {
+			if (!isActive) {
+				setActive(!isActive);
+				setPlaying(!isPlaying);
+			}
+		},
+		[ isPlaying, isActive ]
+	);
+
+	useEffect(() => {
+		function change(slide) {
+			setCurrentSlide(slides.indexOf(slide) + 1);
+		}
+		let slides = document.querySelectorAll('.carousel-slide');
+		slides = [ ...slides ];
+		slides.forEach((slide) => slide.addEventListener('transitionstart', () => change(slide)));
+
+		return () => {
+			slides.forEach((slide) =>
+				slide.removeEventListener('transitionstart', () => change(slide))
+			);
+		};
+	}, []);
+
 	return (
 		<CarouselProvider
 			className="carousel-wrapper"
+			id="carousel"
 			naturalSlideWidth={1920}
 			naturalSlideHeight={941}
-			totalSlides={3}
+			totalSlides={totalSlides}
 			infinite
-			interval={10000}
-			isPlaying={true}
+			interval={interval}
+			isPlaying={isPlaying}
+			visibleSlides={1}
 		>
-			<Slider className="carousel-slider" classNameAnimation="fade-animation">
-				<Slide classNameHidden="hide" classNameVisible="show" index={0}>
+			<Slider
+				className="carousel-slider"
+				classNameAnimation="fade-animation"
+				aria-label="slider"
+			>
+				<Slide
+					className="carousel-slide"
+					classNameHidden="hide"
+					classNameVisible="show"
+					index={0}
+					aria-label="slide"
+				>
 					<StaticImage
 						className="carousel-image"
 						src="../images/Carousel/slide1.webp"
@@ -24,14 +72,26 @@ export default function Carousel() {
 					/>
 				</Slide>
 
-				<Slide classNameHidden="hide" classNameVisible="show" index={1}>
+				<Slide
+					className="carousel-slide"
+					classNameHidden="hide"
+					classNameVisible="show"
+					index={1}
+					aria-label="slide"
+				>
 					<StaticImage
 						className="carousel-image"
 						src="../images/Carousel/slide2.webp"
 						alt="This is the second Image"
 					/>
 				</Slide>
-				<Slide classNameHidden="hide" classNameVisible="show" index={2}>
+				<Slide
+					className="carousel-slide"
+					classNameHidden="hide"
+					classNameVisible="show"
+					index={2}
+					aria-label="slide"
+				>
 					<StaticImage
 						className="carousel-image"
 						src="../images/Carousel/slide3.webp"
@@ -39,22 +99,39 @@ export default function Carousel() {
 					/>
 				</Slide>
 			</Slider>
-			<ButtonBack className="carousel-back-button" aria-label="Right left">
+
+			<ButtonBack
+				id="restart-button"
+				className="carousel-back-button"
+				aria-label="button left"
+				onClick={handleToggle}
+			>
 				<span>
 					<FiArrowLeft className="carousel-arrow-left" />
 				</span>
 			</ButtonBack>
-			<ButtonNext className="carousel-next-button" aria-label="Right right">
+			<ButtonNext
+				onClick={handleToggle}
+				id="restart-button"
+				className="carousel-next-button"
+				aria-label="button right"
+			>
 				<span>
 					<FiArrowRight className="carousel-arrow-right" />
 				</span>
 			</ButtonNext>
-			{/* <div className="progress-bar-container">
-				<div className="progress-bar-item">
-					<h3>Slide 1</h3>
-					<span data-slick-index="0" className="progress-bar" />
-				</div>
-			</div> */}
+			<h4 className="carousel-current">0{currentSlide}</h4>
+			<h4 className="carousel-total">0{totalSlides}</h4>
+			<div className="round-time-bar-back" />
+
+			<div
+				// ref={ref}
+				id="bar"
+				className={isActive ? 'round-time-bar' : null}
+				data-style="smooth"
+			>
+				<div />
+			</div>
 		</CarouselProvider>
 	);
 }
