27909bba270412c5dddb82f191348a04df729537
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-05-24 12:34:06 +0200

    restyle the form using breakpoints

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,17 +2,17 @@ import React from 'react';
 
 export default function Form() {
 	return (
-		<div className="container-fluid form-container">
-			<form action="https://formsubmit.co/patrycja.lesniak@gmail.com" method="POST">
+		<div className="container-fluid form__container">
+			<form className="form__wrapper"action="https://formsubmit.co/patrycja.lesniak@gmail.com" method="POST">
 				<div className="mb-3">
-					<h3 className="form-h3">
+					<h3 className="form__h3">
 						Lorem ipsum <br />Lorem ipsum lorem ipsum
 					</h3>
-					<p className="form-paragraph">consectetur adipiscing elit. Ut auctor arcu</p>
-					<h4 className="form-h4">Zostaw kontakt, zadzwonimy do Ciebie</h4>
+					<p className="form__paragraph">consectetur adipiscing elit. Ut auctor arcu</p>
+					<h4 className="form__h4">Zostaw kontakt, zadzwonimy do Ciebie</h4>
 
 					<input
-						className="form-control form-input"
+						className="form-control form__input"
 						type="text"
 						name="name"
 						required
@@ -21,7 +21,7 @@ export default function Form() {
 					/>
 
 					<input
-						className="form-control form-input"
+						className="form-control form__input"
 						type="text"
 						name="phone"
 						required
@@ -33,34 +33,34 @@ export default function Form() {
 						type="email"
 						name="email"
 						required
-						className="form-control form-input"
+						className="form-control form__input"
 						placeholder="Email"
 						aria-describedby="email form-input"
 					/>
 
 					<input
-						className="form-control form-input"
+						className="form-control form__input"
 						rows="1"
 						name="textarea"
 						required
 						placeholder="Lorem ipsum lorem ipsum"
 						aria-describedby="text input"
 					/>
-					<div className="form-checkbox">
+					<div className="form__checkbox">
 						<input
 							type="checkbox"
 							name="checkbox"
 							required
-							className="form-check-input"
+							className="form__check-input"
 							id="check"
 						/>
-						<div className="form-text">
+						<div>
 							Wyrażam dobrowolną zgodę na przetwarzanie moich danych osobowych
 							więcej...
 						</div>
 					</div>
 
-					<button type="submit" className="form-button"aria-label="Send button">
+					<button type="submit" className="form__button"aria-label="Send button">
 						<span>wyślij</span>
 					</button>
 				</div>
