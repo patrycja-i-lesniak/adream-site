5eb0708e96e9bd64579e74ab2ecb724c3b3a42b7
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-05-24 21:24:43 +0200

    change class names

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,41 +5,41 @@ import { CgMenu } from 'react-icons/cg';
 
 export default function Navbar() {
 	return (
-		<div className="navbar-container">
-			<div className="navbar-logo">
-				<h2 as={Link} className="navbar-logo-h2" to="/">
+		<div className="navbar__container">
+			<div className="navbar__logo">
+				<h2 as={Link} className="navbar__logo-h2" to="/">
 					Logo
 				</h2>
 			</div>
-			<div className="navbar-links">
-				<Link className="navbar-link" to="/">
+			<div className="navbar__links">
+				<Link className="navbar__link" to="/">
 					Poznaj przestrzeń
 				</Link>
 
-				<Link className="navbar-link" to="/offer">
+				<Link className="navbar__link" to="/offer">
 					Oferta
 				</Link>
-				<Link className="navbar-link" to="/location">
+				<Link className="navbar__link" to="/location">
 					Lokalizacja
 				</Link>
 
-				<Link className="navbar-link navbar-break" to="/office">
+				<Link className="navbar__link break" to="/office">
 					Własne biuro
 				</Link>
-				<Link className="navbar-link navbar-break" to="/contact">
+				<Link className="navbar__link break" to="/contact">
 					Kontakt
 				</Link>
 			</div>
-			<div className="navbar-socials">
-				<Link className="navbar-link" to="/">
+			<div className="navbar__socials">
+				<Link className="navbar__link" to="/">
 					<StaticImage src="../static/facebook.png" alt="facebook" />
 				</Link>
-				<Link className="navbar-link" to="/">
+				<Link className="navbar__link" to="/">
 					<StaticImage src="../static/instagram.png" alt="instagram" />
 				</Link>
 			</div>
 			<div>
-				<Link className="navbar-hamburger" to="/">
+				<Link className="navbar__hamburger" to="/">
 					<CgMenu />
 				</Link>
 			</div>
