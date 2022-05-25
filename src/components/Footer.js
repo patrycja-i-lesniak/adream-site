6384315f310f499a48a578ce21b1e2efb968d36e
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-05-25 09:06:40 +0200

    restyle footer, change classnames, delete unnecessary class style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,53 +5,53 @@ import { StaticImage } from 'gatsby-plugin-image';
 export default function Footer() {
 	return (
 		<div className="footer">
-			<div className="footer-container">
-				<div className="footer-graphic">
+			<div className="footer__container">
+				<div className="footer__graphic">
 					<StaticImage
-						className="footer-image"
+						className="footer__image"
 						src="../static/Footer/Group 586@2x.png"
 						alt="graphic"
 					/>
 				</div>
-				<div className="footer-text">
-					<div className="footer-text-right">
-						<h1 className="footer-logo">logo</h1>
-						<p className="footer-paragraph">
+				<div className="footer__text">
+					<div className="footer__text-right">
+						<h1 className="footer__logo">logo</h1>
+						<p className="footer__paragraph">
 							ul. Towarowa 5/6<br />31-000 Kraków
 						</p>
-						<p className="footer-paragraph bold">
+						<p className="footer__paragraph bold">
 							+48 999 999 999<br />email@email.com
 						</p>
-						<p className="footer-policy">Polityka prywatności</p>
+						<p className="footer__policy">Polityka prywatności</p>
 					</div>
 
-					<div className="footer-navigation">
-						<div className="footer-link-box">
-							<Link className="footer-link" to="/">
+					<div className="footer__navigation">
+						<div className="footer__link-box">
+							<Link className="footer__link" to="/">
 								Strona główna
 							</Link>
-							<Link className="footer-link" to="/">
+							<Link className="footer__link" to="/">
 								Poznaj przestrzeń
 							</Link>
-							<Link className="footer-link" to="/offer">
+							<Link className="footer__link" to="/offer">
 								Oferta
 							</Link>
 						</div>
-						<div className="footer-link-box">
-							<Link className="footer-link" to="/location">
+						<div className="footer__link-box">
+							<Link className="footer__link" to="/location">
 								Lokalizacja
 							</Link>
-							<Link className="footer-link" to="/office">
+							<Link className="footer__link" to="/office">
 								Własne biuro
 							</Link>
-							<Link className="footer-link" to="/contact">
+							<Link className="footer__link" to="/contact">
 								Kontakt
 							</Link>
 						</div>
-						<div className="footer-design">
-							<p className="footer-design-text">Proudly designed by</p>
+						<div className="footer__design">
+							<p className="footer__design-text">Proudly designed by</p>
 							<StaticImage
-								className="footer-design-image"
+								className="footer__design-image"
 								src="../static/Footer/logo_adream@2x.png"
 								alt="logo"
 							/>
