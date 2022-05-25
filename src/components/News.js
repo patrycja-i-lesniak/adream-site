087bd50580496bea6401aaa282b970b26fefa43c
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-05-25 10:31:10 +0200

    restyle News component, add hover onmor link

--- src/components/News.js
+++ src/components/News.js
@@ -4,51 +4,51 @@ import { Link } from 'gatsby';
 
 export default function News() {
 	return (
-		<div className='news-container'>
-			<h1 className='news-h1'>Aktualności</h1>
+		<div className="news__container">
+			<h1 className="news__h1">Aktualności</h1>
 
-			<div className='news-wrapper'>
-				<div className='news-box'>
+			<div className="news__wrapper">
+				<div className="news__box">
 					<StaticImage
-						className='news-photo'
+						className="news__photo"
 						src="../images/News/shutterstock_1433594243.png"
 						alt="photo1"
 					/>
-					<p className='news-paragraph'> 01.01.2021</p>
-					<h3 className='news-h3'>
+					<p className="news__paragraph"> 01.01.2021</p>
+					<h3 className="news__h3">
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 					</h3>
-					<p as={Link} className='news-paragraph link' to="/">
+					<p as={Link} className="news__paragraph link" to="/">
 						Więcej
 					</p>
 				</div>
 
-				<div className='news-box'>
+				<div className="news__box">
 					<StaticImage
-						className='news-photo'
+						className="news__photo"
 						src="../images/News/shutterstock_1575692173.png"
 						alt="photo1"
 					/>
-					<p className='news-paragraph'>01.01.2021</p>
-					<h3 className='news-h3'>
+					<p className="news__paragraph">01.01.2021</p>
+					<h3 className="news__h3">
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 					</h3>
-					<p as={Link} className='news-paragraph link' to="/">
+					<p as={Link} className="news__paragraph link" to="/">
 						Więcej
 					</p>
 				</div>
 
-				<div className='news-box'>
+				<div className="news__box">
 					<StaticImage
-						className='news-photo'
+						className="news__photo"
 						src="../images/News/shutterstock_763511722.png"
 						alt="photo1"
 					/>
-					<p className='news-paragraph'>01.01.2021</p>
-					<h3 className='news-h3'>
+					<p className="news__paragraph">01.01.2021</p>
+					<h3 className="news__h3">
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 					</h3>
-					<p as={Link} className='news-paragraph link' to="/">
+					<p as={Link} className="news__paragraph link" to="/">
 						Więcej
 					</p>
 				</div>
